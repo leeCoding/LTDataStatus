ca9fc372f1b3ee039b459fd1b0a5ba601a5468ad
Jonny_Lee <nscode@sina.com>
2016-10-29 20:30:11 +0800

    修改注释

--- LTDataStatus/ViewController.m
+++ LTDataStatus/ViewController.m
@@ -19,27 +19,37 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    // 1.显示文字
-//    [self showStatus:@"没有数据哦！" tapViewWithBlock:^{
-//        NSLog(@"点击屏幕了");
-//    }];
     
-    // 2.显示图片加文字
-    [self showStatus:@"没有数据哦！" imageName:@"no_data.jpg" type:nil tapViewWithBlock:^{
+    /* 1.显示文字
+    [self showStatus:@"没有数据哦！点击屏幕加载..." tapViewWithBlock:^{
         NSLog(@"点击屏幕了");
     }];
-//
-//    // 3.显示gif加文字
-//    [self showStatus:@"没有数据哦！" imageName:@"gif" type:@"gif" tapViewWithBlock:^{
-//        NSLog(@"点击屏幕了");
-//    }];
-
+    */
     
-    // 隐藏
+    /* 2.显示图片加文字
+    [self showStatus:@"没有数据哦！点击屏幕加载..." imageName:@"no_data.jpg" type:nil tapViewWithBlock:^{
+        NSLog(@"点击屏幕了");
+    }];
+     */
+    
+    /* 3.显示gif加文字
+    [self showStatus:@"没有数据哦！点击屏幕加载..." imageName:@"gif" type:@"gif" tapViewWithBlock:^{
+        NSLog(@"点击屏幕了");
+    }];
+    */
+    
+    // 4.显示gif
+    [self showStatus:nil imageName:@"gif" type:@"gif" tapViewWithBlock:^{
+        NSLog(@"点击屏幕了");
+    }];
+    
+    
+    // 隐藏，三秒后消失
     __weak typeof(self) __weakSelf = self;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [__weakSelf hide];
     });
+    
 }
 
 
